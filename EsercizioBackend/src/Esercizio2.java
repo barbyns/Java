e37b1ebb9500ea499c8f99eac1f379804f5ff688
barbyns <barbyns95@gmail.com>
2025-05-05 17:00:35 +0200

    Update Esercizio2.java

--- EsercizioBackend/src/Esercizio2.java
+++ EsercizioBackend/src/Esercizio2.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
 
-public class Esercizio2 { public static void main(String[]args){
-    Scanner scanner= new Scanner(System.in);
-    System.out.println("Inserisci una stringa, Hello");
-    String a = scanner.next();
-    System.out.println("Inserisci un'altra stringa, Hiu");
-    String b = scanner.next();
-    System.out.println("Inserisci un'altra stringa ancora, Maddonna");
-    String c = scanner.next();
-    String TreStringhe= concat(a,b,c);
-    System.out.println(TreStringhe);
-}
-public static String concat (String a, String b, String c){
-    return a+b+c;
-}
+public class Esercizio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci la prima stringa:");
+        String a = scanner.nextLine();
+
+        System.out.println("Inserisci la seconda stringa:");
+        String b = scanner.nextLine();
+
+        System.out.println("Inserisci la terza stringa:");
+        String c = scanner.nextLine();
+
+        String treStringhe = concat(a, b, c);
+        System.out.println("Stringa concatenata: " + treStringhe);
+    }
+
+    public static String concat(String a, String b, String c) {
+        return a + b + c;
+    }
 }
